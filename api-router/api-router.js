113979c850388304d7db146ab5898a9e58c0264e
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-12-15 15:04:26 -0500

    removes session storage

--- api-router/api-router.js
+++ api-router/api-router.js
@@ -21,7 +21,6 @@ router.post('/login', async (req, res) => {
         let existing = await Users.findByUsername(req.body.username)
         if (existing.password === req.body.password) {
             req.session.user = existing;
-            sessionStorage.setItem('session', req.session)
             res.status(200).json(`welcome, ${req.session.user.username}`)
         } else {
             res.status(400).json({ err: 'invalid creds'})
